41ceb619bef59c2a512637c5dc50fe976454a6ac
caroSeminario23 <carolina.seminario@unmsm.edu.pe>
2024-12-02 00:26:40 -0500

    Frontend - Conexión con listas de reproducción de Youtube para cada tipo de residuo

---
+++ frontend/scripts.js
@@ -0,0 +1,39 @@
+// lista de reproduccion 1 - plástico
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.querySelector('.bloque5-boton1-recuadro');
+    if (button) {
+      button.addEventListener('click', () => {
+        window.open('https://youtube.com/playlist?list=PLDje3QcKQLjVoEPhtcBt2xhlMI1-rTkqZ&si=rdk1ahvYL5lp-S70', '_blank');
+      });
+    }
+});
+
+// lista de reproduccion 2 - papel y cartón
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.querySelector('.bloque5-boton2-recuadro');
+    if (button) {
+      button.addEventListener('click', () => {
+        window.open('https://youtube.com/playlist?list=PLDje3QcKQLjWXCyg38-rlQLyrzw37hS4n&si=SPjt6IlhUUs0Ffm6', '_blank');
+      });
+    }
+});
+
+// lista de reproduccion 3 - vidrio
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.querySelector('.bloque5-boton3-recuadro');
+    if (button) {
+      button.addEventListener('click', () => {
+        window.open('https://youtube.com/playlist?list=PLDje3QcKQLjV_a3OUJQW0EpxT-b9kb0ce&si=7UuSj4t_x_HJqjbX', '_blank');
+      });
+    }
+});
+
+// lista de reproduccion 4 - metal
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.querySelector('.bloque5-boton4-recuadro');
+    if (button) {
+      button.addEventListener('click', () => {
+        window.open('https://youtube.com/playlist?list=PLDje3QcKQLjV6PUDAlVdmGZWVx-5bQfI9&si=N9T1wgAgT3_dyncD', '_blank');
+      });
+    }
+});
